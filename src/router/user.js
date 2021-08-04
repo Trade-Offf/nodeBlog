6f31922bd820ac131c922c5a83ebe321caad8ed7
jia_lin.li <jia_lin.li@ceasia.cn>
2021-08-04 11:34:44 +0800

    first commmit

---
+++ src/router/user.js
@@ -0,0 +1,12 @@
+const handleUserRouter = (req, res) => {
+  const method = req.method;
+
+  // 登陆接口
+  if (method === "POST" && req.path === "/api/user/login") {
+    return {
+      msg: "这是登陆接口",
+    };
+  }
+};
+
+module.exports = handleUserRouter;
